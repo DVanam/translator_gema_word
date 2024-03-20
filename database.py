880ceca1ed3b680c9e9ebc965b1125f7645232ad
DVanam <hisokadara@gmail.com>
2024-03-20 22:46:07 +0300

    add directory git. add json save file. add json load file. add load database to textFiled add load words in json and db add comit

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import json
 import sqlite3
 import random
-
+from datetime import datetime
 
 class Database:
     def __init__(self, db_name):
@@ -9,26 +9,61 @@ class Database:
         self.cursor = self.connection.cursor()
         self.create_table()
 
-        self.words = []
+        self.words_list = []
+        self.words_dict = dict()
 
     def create_table(self):
         """Создаем таблицу в базе данных для хранения данных."""
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS saved_data (
-                                id INTEGER PRIMARY KEY,
-                                data TEXT
-                                )''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS words
+                                       (id INTEGER PRIMARY KEY,
+                                       english TEXT,
+                                       russian TEXT)''')
         self.connection.commit()
 
-    def insert_data(self, data):
-        data_json = json.dumps(data)
-        self.cursor.execute("INSERT INTO saved_data (data) VALUES (?)", (data_json,))
+    def add_word_in_db(self, english, russian):
+        """Добавление в таблицу базы даныз"""
+        self.cursor.execute("INSERT INTO words (english, russian) VALUES (?, ?)", (english, russian))
         self.connection.commit()
 
-    def get_random_word(self):
-        return random.choice(self.words) if self.words else ("", "")
+    def save_to_json(self, filename):
+        """Создание json file in database"""
+        self.cursor.execute("SELECT * FROM words")
+        words = self.cursor.fetchall()
+        word_dict = [{"russian": row[1], "english": row[2]} for row in words]
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(word_dict, f, indent=2)
 
-    def add_word(self, english, russian):
+    def load_from_json(self, filename):
+        """Загрузка json file. Чере json.load."""
+        with open(filename, "r") as f:
+            word_dict = json.load(f)
+            for i in range(len(word_dict)):
+                ru = word_dict[i]["russian"]
+                en = word_dict[i]["english"]
+                # setdefault -Уникальных значений. Без повторов.
+                self.words_dict.setdefault(ru, en)  # dict
 
-        self.words.append((english, russian))
+
+    def get_table_db(self):
+        # self.cursor.execute("SELECT * FROM words ORDER BY RANDOM() LIMIT 1")
+        self.cursor.execute("SELECT * FROM words")
+        word_rand_db = self.cursor.fetchall()
+        all_table_db = []
+        for item in range(len(word_rand_db)):
+            all_table_db.append(word_rand_db[item])
+        return all_table_db
+
+    def get_last_data(self):
+        """Получаем последний сохраненный набор данных из базы данных."""
+        self.cursor.execute("SELECT * FROM words ORDER BY id DESC LIMIT 1")
+        rowd = self.cursor.fetchone()
+        if rowd:
+            return rowd[1], rowd[2]
+        # return json.loads(result[0]) if result else []
+
+    def __del__(self):
+        """Close the database connection when the object is deleted."""
+        self.connection.close()
+
 
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import random
-import json
+from datetime import datetime
+import csv
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow
 from googletrans import Translator
@@ -19,11 +20,10 @@ class TranslatorApp(QMainWindow):
 
         self.input_text = self.ui.input_text
         self.output_text = self.ui.output_text
-
+        self.input_text.textChanged.connect(self.translate_text)
 
         self.en_text = self.ui.en_le_text
         self.en_text.returnPressed.connect(self.check_answer)
-
         self.ru_text = self.ui.ru_qeustion
 
         self.history_field = self.ui.check_qeust_ans_text
@@ -32,38 +32,44 @@ class TranslatorApp(QMainWindow):
         self.start_button = self.ui.start_session
         self.start_button.clicked.connect(self.start_session)
         self.add_new_btn = self.ui.save_word_button
-        self.add_new_btn.clicked.connect(self.open_window_add_words)
-
+        self.add_new_btn.clicked.connect(self.add_words_in_file_js_db)
 
         self.translator = Translator()
 
         self.database = Database('trans_database.db')
+        self.load_table_database()
 
-        self.input_text.textChanged.connect(self.translate_text)
-
-        self.words = {
-            "привет": "hello",
-            "пока": "goodbye",
-            "спасибо": "thank you",
-            # Add more words here
-        }
+        self.database.load_from_json('words_js_file.json')
+        self.words = self.database.words_dict
 
         self.session_words = []
         self.current_word = None
         # self.show()
 
-    def open_window_add_words(self):
-        self.new_window = QtWidgets.QDialog()
-        self.ui_window = Ui_Dialog()
-        self.ui_window.setupUi(self.new_window)
-        self.new_window.show()
-        sender = self.sender()
+    def load_table_database(self):
+        """Загрузка и отображение таблицы из сохраненных данных из базы данных."""
+        get_random = self.database.get_table_db()
+        all_table_db = []
+        for item in range(len(get_random)):
+            all_table_db.append(get_random[item])
+        self.history_field.append(str(all_table_db))
+
+    def add_words_in_file_js_db(self):
+        """"Добавление в фаил jsan, database, вывод сообщения при бодавления."""
+        russian = self.input_text.toPlainText()
+        english = self.output_text.toPlainText()
+        if russian:
+            self.database.add_word(russian, english)
+            self.history_field.setText(f"Added to the databasie: {russian, english}")
+        self.database.save_to_json('words_js_file.json')
 
     def start_session(self):
+        """Старт ссесии. Выбор количества вопросов в рандом"""
         self.session_words = random.sample(list(self.words.items()), self.session_spin.value())
         self.next_word()
 
     def next_word(self):
+        """Следующий вопрос из сесии. Выбор корретного ответа. Удаление из переменой с вопросами для ссеси. Вывод сообщении об окончание ссесии """
         if not self.session_words:
             self.history_field.append("End of session. Start a new session.")
             self.ru_text.clear()
@@ -72,6 +78,7 @@ class TranslatorApp(QMainWindow):
         self.ru_text.setText(self.current_word[0])
 
     def check_answer(self):
+        """Проверка на ответ пользователя запущеной ссесие. Вывод информации вопрос ответ."""
         user_answer = self.en_text.text().strip()
         correct_answer = self.current_word[1]
 
@@ -85,6 +92,7 @@ class TranslatorApp(QMainWindow):
         self.next_word()
 
     def translate_text(self):
+        """"Преревод текста"""
         input_text = self.input_text.toPlainText()
         if input_text.strip():  # Проверяем, что введенный текст не пустой
             translated_text = self.translator.translate(input_text, src='ru', dest='en').text
